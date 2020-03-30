8eea0336e040cab3e686579aa92ae49bd2b294d3
JayShiznit <hc.boba.contact@gmail.com>
2020-03-30 22:31:31 +0200

    Added inventory-app project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Inventory App
+
+An app to keep track of your inventory of items.
+
+## Backend Setup
+
+* Install dependencies: `npm install`
+* Create `.env` with your values (see `.env.sample` file)
+* Run the postgres db / pgadmin4: `docker-compose up`
+* Migrate the database: `npm run migrate`
---
+++ backend/src/constants/tableNames.js
@@ -0,0 +1,15 @@
+module.exports = {
+  user: "user",
+  item_type: "item_type",
+  state: "state",
+  country: "country",
+  shape: "shape",
+  location: "location",
+  address: "address",
+  manufacturer: "manufacturer",
+  size: "size",
+  item: "item",
+  item_info: "item_info",
+  item_image: "item_image",
+  related_item: "related_item"
+};
